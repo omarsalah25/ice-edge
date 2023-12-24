704ec325a4307ad67683584c4d2f2f9862539634
omarsalah25 <55956352+omarsalah25@users.noreply.github.com>
2023-12-24 12:25:38 +0200

    up

--- modules/cimo/assets/scripts/min.js
+++ modules/cimo/assets/scripts/min.js
@@ -931,3 +931,143 @@ if (!Modernizr.svg) {
 		$this.attr("src", $(this).attr("src").replace('.svg', '.' + data_svg));
 	});
 }
+
+
+$('.slider').each(function () {
+	var $this = $(this);
+	var $group = $this.find('.slide_group');
+	var $slides = $this.find('.slide');
+	var bulletArray = [];
+	var currentIndex = 0;
+	var timeout;
+
+	function move(newIndex) {
+		var animateLeft, slideLeft;
+
+		advance();
+
+		if ($group.is(':animated') || currentIndex === newIndex) {
+			return;
+		}
+
+		bulletArray[currentIndex].removeClass('active');
+		bulletArray[newIndex].addClass('active');
+
+		if (newIndex > currentIndex) {
+			slideLeft = '100%';
+			animateLeft = '-100%';
+		} else {
+			slideLeft = '-100%';
+			animateLeft = '100%';
+		}
+
+		$slides.eq(newIndex).css({
+			display: 'block',
+			left: slideLeft
+		});
+		$group.animate({
+			left: animateLeft
+		}, function () {
+			$slides.eq(currentIndex).css({
+				display: 'none'
+			});
+			$slides.eq(newIndex).css({
+				left: 0
+			});
+			$group.css({
+				left: 0
+			});
+			currentIndex = newIndex;
+		});
+	}
+
+	function advance() {
+		clearTimeout(timeout);
+		timeout = setTimeout(function () {
+			if (currentIndex < ($slides.length - 1)) {
+				move(currentIndex + 1);
+			} else {
+				move(0);
+			}
+		}, 4000);
+	}
+
+	$('.next_btn').on('click', function () {
+		if (currentIndex < ($slides.length - 1)) {
+			move(currentIndex + 1);
+		} else {
+			move(0);
+		}
+	});
+
+	$('.previous_btn').on('click', function () {
+		if (currentIndex !== 0) {
+			move(currentIndex - 1);
+		} else {
+			move(3);
+		}
+	});
+
+	$.each($slides, function (index) {
+		var $button = $('<a class="slide_btn">&bull;</a>');
+
+		if (index === currentIndex) {
+			$button.addClass('active');
+		}
+		$button.on('click', function () {
+			move(index);
+		}).appendTo('.slide_buttons');
+		bulletArray.push($button);
+	});
+
+	advance();
+});
+
+$('.responsive').slick({
+	infinite: true,
+	autoplay: true,
+	loop: true,
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	prevArrow: false,
+	nextArrow: false,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	]
+});
+
+
+
+
+
+
+
+
+
+
+
